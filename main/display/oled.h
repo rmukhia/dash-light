2dfa625f46fad6fb9a950c61a11ca7376ee2bb2c
Raunak Mukhia <raunak.mukhia@gmail.com>
2022-06-20 22:04:11 +0700

    Made fft into component.

---
+++ main/display/oled.h
@@ -0,0 +1,32 @@
+//
+// Created by rmukhia on 18/6/22.
+//
+
+#ifndef LIGHT_BURST_OLED_H
+#define LIGHT_BURST_OLED_H
+#include "freqb.h"
+
+typedef enum display_mode_e {
+    DISPLAY_MODE_NONE,
+    DISPLAY_MODE_MENU,
+    DISPLAY_MODE_EQ,
+} display_mode_t;
+
+typedef struct display_params_eq_s {
+    int num_bands;
+    float bands[FREQB_MAX_BANDS];
+} display_params_eq_t;
+
+typedef struct display_param_s {
+    display_mode_t mode;
+    union {
+        display_params_eq_t eq_params;
+    };
+} display_param_t;
+
+esp_err_t display_oled_init();
+
+esp_err_t display_update(display_param_t param);
+
+
+#endif //LIGHT_BURST_OLED_H
